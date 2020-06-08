937746e12e6f5febc592c824d0d192e23b30cb64
Toan Nguyen <toan.nguyen@employmenthero.com>
2020-06-08 23:45:25 +0700

    config webpack

--- playground/src/index.stories.js
+++ playground/src/index.stories.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Greeting from 'hero-editor';
+import HeroEditor from 'hero-editor';
 
 export default { title: 'Hero Editor' };
 
-export const interactive = () => <Greeting />;
+export const interactive = () => <HeroEditor />;
 
