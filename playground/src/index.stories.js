ac539869cb716cc387a4bf7f844bc5bbfc1669c6
Toan Nguyen <toan.nguyen@employmenthero.com>
2020-06-11 16:29:04 +0700

    add basic mark support

--- playground/src/index.stories.js
+++ playground/src/index.stories.js
@@ -1,6 +1,37 @@
-import React from 'react';
-import HeroEditor from 'hero-editor';
+import React, { useState, useMemo } from 'react';
+import HeroEditor, { JsonViewer, bold, italic, underline } from 'hero-editor';
 
 export default { title: 'Hero Editor' };
 
-export const interactive = () => <HeroEditor />;
+export const interactive = () => {
+  const plugins = useMemo(() => [bold, italic, underline], []);
+  const [value, setValue] = useState([
+    {
+      type: 'paragraph',
+      children: [{ text: 'A line of text in a paragraph.' }],
+    },
+    {
+      type: 'paragraph',
+      children: [
+        { text: 'This is editable ' },
+        { text: 'rich', bold: true },
+        { text: ' text, ' },
+        { text: 'much', italic: true },
+        { text: ' better than a ' },
+        { text: '<textarea>', underline: true },
+        { text: '!' },
+      ],
+    },
+  ]);
+
+  return (
+    <div>
+      <HeroEditor
+        plugins={plugins}
+        value={value}
+        onChange={(newValue) => setValue(newValue)}
+      ></HeroEditor>
+      <JsonViewer value={value} />
+    </div>
+  );
+};
